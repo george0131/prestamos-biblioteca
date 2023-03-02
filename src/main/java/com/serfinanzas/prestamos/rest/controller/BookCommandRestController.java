cd09db578e5a41b4176faaaba001c1a085a58307
Jorge <jorgeibanez7@gmail.com>
2023-03-02 00:23:09 -0500

    api para crear libros

---
+++ src/main/java/com/serfinanzas/prestamos/rest/controller/BookCommandRestController.java
@@ -0,0 +1,28 @@
+package com.serfinanzas.prestamos.rest.controller;
+
+import com.serfinanzas.prestamos.persistence.domain.Book;
+import com.serfinanzas.prestamos.rest.CommandRestController;
+import com.serfinanzas.prestamos.rest.domain.BookInput;
+import com.serfinanzas.prestamos.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BookCommandRestController extends CommandRestController {
+
+    private final BookService service;
+
+    public BookCommandRestController(BookService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/book/create")
+    public ResponseEntity<Book> createBook(BookInput input) {
+
+        return new ResponseEntity<>(service.createBook(input), HttpStatus.CREATED);
+    }
+}
